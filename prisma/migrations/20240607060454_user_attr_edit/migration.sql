e531a31631a1c90ef540f09e8d1c8425f6ad7d27
liberaldev <j0321@mju.ac.kr>
2024-06-07 15:52:06 +0900

    user attr edit

---
+++ prisma/migrations/20240607060454_user_attr_edit/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `role` on the `user` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `user` DROP COLUMN `role`,
+    ADD COLUMN `email_verified` BOOLEAN NOT NULL DEFAULT false;
