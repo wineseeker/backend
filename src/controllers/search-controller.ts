2b6152036d86d6554512a0b4000360c68769111d
liberaldev <j0321@mju.ac.kr>
2024-06-08 15:16:14 +0900

    자동완성 기능 추가

--- src/controllers/search-controller.ts
+++ src/controllers/search-controller.ts
@@ -21,3 +21,33 @@ export async function search(req: Request, res: Response) {
 
     res.status(200).json(results);
 }
+
+export async function autoComplete(req: Request, res: Response) {
+    if (typeof req.query.q !== "string")
+        return res.status(204).send()
+
+    let results = await prisma.wines.findMany({
+        take: 7,
+        where: {
+            name: {
+                startsWith: req.query.q.toString(),
+            }
+        }
+    })
+
+    if (results.length < 7) {
+        const contains = await prisma.wines.findMany({
+            take: 7 - results.length,
+            where: {
+                name: {
+                    contains: req.query.q.toString(),
+                }
+            }
+        })
+
+        if (contains.length > 0)
+            results = [...results, ...contains]
+    }
+
+    res.status(200).json(results);
+}
--- src/routes/search.ts
+++ src/routes/search.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import {search} from "../controllers/search-controller.js";
+import {autoComplete, search} from "../controllers/search-controller.js";
 
 const router: Router = Router();
 
 router.get('/', search);
+
+router.get('/auto-complete', autoComplete);
 
 export default router;
