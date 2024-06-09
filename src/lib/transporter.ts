f21a01e1e309bd33d23e1cc985bed11cc7c42fb1
liberaldev <j0321@mju.ac.kr>
2024-06-09 14:57:01 +0900

    SMTP 관련 환경변수 불러오도록 수정 및 .env.sample에 반영

--- src/lib/transporter.ts
+++ src/lib/transporter.ts
@@ -1,7 +1,15 @@
 import nodemailer from "nodemailer";
 
+const smtpHost = process.env.SMTP_HOST ? process.env.SMTP_HOST : "localhost"
+const smtpSecure: boolean = process.env.SMTP_SECURE ? Boolean(process.env.SMTP_SECURE) : false;
+const smtpPort = process.env.SMTP_PORT ? parseInt(process.env.SMTP_PORT, 10) : (smtpSecure ? 465 : 1025);
+
 export const transporter = nodemailer.createTransport({
-    host: "127.0.0.1",
-    port: 1025,
-    secure: false, // Use `true` for port 465, `false` for all other ports
+    host: smtpHost,
+    port: smtpPort,
+    secure: smtpSecure,
+    auth: {
+        user: process.env.SMTP_USER ? process.env.SMTP_USER : undefined,
+        pass: process.env.SMTP_PASSWORD ? process.env.SMTP_PASSWORD : undefined,
+    }
 });
