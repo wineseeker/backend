07c1eece6f1d835a10046dc6bac0c051746652d3
liberaldev <j0321@mju.ac.kr>
2024-06-10 09:54:29 +0900

    배포 오류 방지

--- shell-scripts/after_install.sh
+++ shell-scripts/after_install.sh
@@ -8,3 +8,5 @@ echo SMTP_PORT=$(aws ssm get-parameters --names $path"/wine-seeker/backend/produ
 echo SMTP_SECURE=$(aws ssm get-parameters --names $path"/wine-seeker/backend/production/SMTP_SECURE" --query Parameters[0].Value | sed 's/"//g') >> $filename
 echo SMTP_USER=$(aws ssm get-parameters --names $path"/wine-seeker/backend/production/SMTP_USER" --query Parameters[0].Value | sed 's/"//g') >> $filename
 echo SMTP_PASSWORD=$(aws ssm get-parameters --names $path"/wine-seeker/backend/production/SMTP_PASSWORD" --query Parameters[0].Value | sed 's/"//g') >> $filename
+
+npm ci
